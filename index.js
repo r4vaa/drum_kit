89be72933800c6ce987591185f03a0fff1d6fcc7
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-02-16 20:56:42 +0530

    added keyboard events

--- index.js
+++ index.js
@@ -3,44 +3,7 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 for( var i = 0; i < numberOfButtons; i++){
     document.querySelectorAll("button")[i].addEventListener("click", function (){
         buttonInnerHtml = (this.innerHTML)
-      switch (buttonInnerHtml) {
-          case "w" :
-            var tom1= new Audio('./sounds/tom-1.mp3');
-                tom1.play();
-              break;
-              
-          case "a" :
-            var tom2= new Audio('./sounds/tom-2.mp3');
-                tom2.play();
-             break;
-
-          case "s" :
-            var tom3= new Audio('./sounds/tom-3.mp3');
-                tom3.play();
-              break;
-
-          case "d" :
-             var tom4= new Audio('./sounds/tom-4.mp3');
-                tom4.play();
-              break;
-
-          case "j" :
-            var snare= new Audio('./sounds/snare.mp3');
-                snare.play();
-              break;
-
-          case "k" :
-            var crash= new Audio('./sounds/crash.mp3');
-                crash.play();
-              break;
-
-          case "l" :
-            var kick= new Audio('./sounds/kick-bass.mp3');
-                kick.play();
-              break;
-          default:
-              console.log("buttonInnerHtml");
-      }
+        makeSound(buttonInnerHtml);
 
         //what to do when click detected
         
@@ -48,5 +11,52 @@ for( var i = 0; i < numberOfButtons; i++){
 
 }
 
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+})
 
 
+function makeSound(key){
+
+  switch (key) {
+    case "w" :
+      var tom1= new Audio('./sounds/tom-1.mp3'); 
+          tom1.play();
+        break;
+        //tap into the audio hardware
+        //check the file at fileloaction exists
+        //check the file at fileloaction is a sound file
+        //play the file at filelocation.
+    case "a" :
+      var tom2= new Audio('./sounds/tom-2.mp3');
+          tom2.play();
+       break;
+
+    case "s" :
+      var tom3= new Audio('./sounds/tom-3.mp3');
+          tom3.play();
+        break;
+
+    case "d" :
+       var tom4= new Audio('./sounds/tom-4.mp3');
+          tom4.play();
+        break;
+
+    case "j" :
+      var snare= new Audio('./sounds/snare.mp3');
+          snare.play();
+        break;
+
+    case "k" :
+      var crash= new Audio('./sounds/crash.mp3');
+          crash.play();
+        break;
+
+    case "l" :
+      var kick= new Audio('./sounds/kick-bass.mp3');
+          kick.play();
+        break;
+    default:
+        console.log("buttonInnerHtml");
+}
+}
